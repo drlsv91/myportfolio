d4bc3380d020ba01e48af14009f2c0695602c101
Oluwaleye Victor <oluwasquarelsv@gmail.com>
2021-04-04 19:25:29 +0100

    more styling

--- src/components/profile/aboutme.jsx
+++ src/components/profile/aboutme.jsx
@@ -5,18 +5,24 @@ import Typewriter from 'typewriter-effect'
 const AboutMe = () => {
   return (
     <Col md={6} xl={6} sm={12} className="mobile-padding">
-      <h1 className="font-size-25px">
+      {/* <CustomTypingEffect /> */}
+      <h1 className="font-size-25px ">
         <Typewriter
           options={{
             strings: ['Oluwaleye Victor'],
             autoStart: true,
             loop: true,
+            cursor: '_',
+            cursorClassName: 'cursor',
+            wrapperClassName: 'typing-wrapper',
           }}
         />
       </h1>
-      <h3 className="font-size-18px mobile-font">Developer</h3>
-      <p>
-        I am a full stack developer with strong desire for self-improvement.
+      <h3 className="font-size-18px mobile-font bg-deep-yellow medium-title">
+        Web Developer
+      </h3>
+      <p className="text-left noSelect">
+        I am a full stack developer with a strong desire for self-improvement.
         patient, a problem solver, an optimist and have strong social values
         that will be beneficial to a team
       </p>
